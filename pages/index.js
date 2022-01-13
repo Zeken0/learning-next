a0b2fb17caf6fd95a573084429c45d62728ffb7b
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-01-13 11:26:13 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,26 @@ import styles from "../styles/Home.module.css";
 import List from "../components/list/list";
 import Title from "../components/title/title";
 import CustomButton from "../components/button/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [direction, setUpdatedDirection] = useState("down");
+
+  const handleOnClick = (directionWanted) => {
+    setUpdatedDirection(directionWanted);
+  };
   return (
     <div className={styles.container}>
       <Title customColor="red">Awesome example</Title>
+      <p>{direction}</p>
       <List />
-      <CustomButton>Click me</CustomButton>
+      <CustomButton
+        onClick={() => {
+          handleOnClick("up");
+        }}
+      >
+        Click me
+      </CustomButton>
     </div>
   );
 }
