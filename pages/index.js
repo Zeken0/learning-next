216acafb51ee64808fe0e261b1789d2b9ba36f0b
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-01-14 10:31:43 +0100

    L

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from "../styles/Home.module.css";
 import List from "../components/list/list";
 import Title from "../components/title/title";
 import CustomButton from "../components/button/button";
+import Counter from "../components/counter/counter";
 import { useState } from "react";
 
 export default function Home() {
@@ -22,6 +23,7 @@ export default function Home() {
       >
         Click me
       </CustomButton>
+      <Counter />
     </div>
   );
 }
