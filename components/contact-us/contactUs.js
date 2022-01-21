7b859d9f02fb1845e70e0c7047eb032f9000dd7e
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-01-21 13:01:45 +0100

    form

--- components/contact-us/contactUs.js
+++ components/contact-us/contactUs.js
@@ -1,7 +1,17 @@
 import { useFormik } from "formik";
+import * as yup from "yup";
 
 export default function ContactUs() {
-  const { values, handleSubmit, handleChange, errors } = useFormik({
+  const {
+    values,
+    handleSubmit,
+    handleChange,
+    errors,
+    touched,
+    handleBlur,
+    resetForm,
+    isSubmitting,
+  } = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
@@ -10,15 +20,18 @@ export default function ContactUs() {
       message: "",
     },
 
-    validate: (values) => {
-      const errors = {};
-      if (!values.firstName) {
-        errors.firstName = "Required";
-      } else if (values.firstName.length < 2) {
-        errors.firstName = "Please dont lie";
-      }
-      return errors;
-    },
+    validationSchema: yup.object({
+      firstName: yup.string().min(2, "Too short!").required("Required!"),
+      lastName: yup.string().min(2, "Too short!").required("Required!"),
+      email: yup.string().email("Invalid emal address!").required("Required!"),
+      musicTaste: yup.string().required("Required!"),
+      message: yup
+        .string()
+        .min(2, "Too short!")
+        .max(200, "we dont need a story")
+        .trim()
+        .required("Required!"),
+    }),
 
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
@@ -34,47 +47,63 @@ export default function ContactUs() {
           placeholder="Ahmed"
           value={values.firstName}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
-        <p style={{ color: "red" }}>{errors.firstName}</p>
-        <br />
+        <p style={{ color: "red" }}>{touched.firstName && errors.firstName}</p>
+
         <input
           id="lastName"
           name="lastName"
           placeholder="Jibril"
           value={values.lastName}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
-        <br />
+        <p style={{ color: "red" }}>{touched.lastName && errors.lastName}</p>
+
         <input
           id="email"
           name="email"
           placeholder="blabla@live.com"
           value={values.email}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
-        <br />
+        <p style={{ color: "red" }}>{touched.email && errors.email}</p>
+
         <select
           id="musicTaste"
           name="musicTaste"
           value={values.musicTaste}
           onChange={handleChange}
+          onBlur={handleBlur}
         >
-          <option value="default">Music</option>
+          <option value="default">Genre</option>
           <option value="Classical">Classical</option>
           <option value="Rap">Rap</option>
           <option value="R&B">R&B</option>
           <option value="Pop">Pop</option>
         </select>
-        <br />
+        <p style={{ color: "red" }}>
+          {touched.musicTaste && errors.musicTaste}
+        </p>
+
         <textarea
           id="message"
           name="message"
           placeholder="say somthing"
           value={values.message}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
-        <br />
-        <button type="submit">Submit</button>
+        <p style={{ color: "red" }}>{touched.message && errors.message}</p>
+
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+        <button style={{ color: "red" }} type="reset" onClick={resetForm}>
+          Reset
+        </button>
       </form>
     </div>
   );
